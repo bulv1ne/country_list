9eaa6be132fddae9275072866ce2938d9cb668ce
Niels Lemmens <draso.odin@gmail.com>
2019-11-14 11:47:13 +0100

    Bump version: 0.1.3 → 0.1.4

--- setup.py
+++ setup.py
@@ -28,6 +28,6 @@ setup(
     name="country_list",
     packages=find_packages(include=["country_list"]),
     url="https://github.com/bulv1ne/country_list",
-    version="0.1.3",
+    version="0.1.4",
     zip_safe=False,
 )
