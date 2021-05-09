ab1db08d00ddef3e04d9ca4a138f06e11c654253
Niels Lemmens <draso.odin@gmail.com>
2021-05-09 06:31:33 +0200

    Update packages and add python 3.9 for testing

--- setup.py
+++ setup.py
@@ -15,9 +15,9 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     description="List of all countries with names and ISO 3166-1 codes "
     "in all languages",
